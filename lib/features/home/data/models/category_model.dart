8dd55c474e0189b21ba4d925e8ec34fef5b37097
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2025-12-03 09:15:16 +0300

    Add home feature data models and update analysis options
    - Create `CategoryModel` and `CategoryImage` using `freezed` for type-safe category data handling
    - Create `QuestionModel` using `freezed` to represent question data with JSON serialization support
    - Update `analysis_options.yaml` to ignore `invalid_annotation_target` errors, facilitating code generation
    - Minor formatting adjustments in `onboarding_cubit.dart` and `onboarding_state.dart`

---
+++ lib/features/home/data/models/category_model.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'category_model.freezed.dart';
+part 'category_model.g.dart';
+
+@freezed
+abstract class CategoryModel with _$CategoryModel {
+  const factory CategoryModel({
+    required int id,
+    required String name,
+    required String title,
+    required int rank,
+    required CategoryImage? image,
+  }) = _CategoryModel;
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) =>
+      _$CategoryModelFromJson(json);
+}
+
+@freezed
+abstract class CategoryImage with _$CategoryImage {
+  const factory CategoryImage({
+    required int id,
+    required String url,
+  }) = _CategoryImage;
+
+  factory CategoryImage.fromJson(Map<String, dynamic> json) =>
+      _$CategoryImageFromJson(json);
+}
